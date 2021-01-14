0050bffef9f7b5d613874400bb380f5f5482c6f4
Nux007 <007.nux@gmail.com>
2021-01-14 06:11:10 +0100

    Exo 137.

---
+++ if-simple.php
@@ -0,0 +1,43 @@
+<?php
+/*****************************************/
+/********** Conditions Simples ***********/
+/*****************************************/
+//### Exemple ###//
+echo "CONDITIONS SIMPLES <br><br>";
+
+$animal = "Chat";
+// La condition doit répondre à la question:
+echo "1.Est-ce un chat : ";
+
+if($animal == "Chat") {
+  //condition vraie
+  echo "Oui";
+}
+else {
+  //condition fausse
+  echo "Non";
+}
+echo "<br><br>";
+
+
+/*###############################################*/
+/*################# ACTIVITÉS ###################*/
+/*###############################################*/
+// Écrire la condition qui affiche "Non" quand $chat vaut "Chien" et affiche "Oui" quand $chat vaut "Chat" ###//
+$animal = "Chien";
+echo "2.Est-ce un chat : ";
+
+// TODO mettre votre code ici
+
+echo "<br><br>";
+
+//----------------------------------------
+// Écrire la condition qui affiche "Oui" quand $chat vaut true et affiche "Non" dans le cas contraire ###//
+$chat = true;
+echo "3.Est-ce un chat : ";
+
+// TODO mettre votre code ici
+
+echo "<br><br>";
+
+
